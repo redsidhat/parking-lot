63ca1e4743f70e44bf6ebcbe42b19729f1e0f5fe
terminalninja <sidsidhardh@gmail.com>
2023-04-07 18:00:35 +0530

    changed aguments to read from file and changed output messages to match requirement

--- parkinglot.py
+++ parkinglot.py
@@ -41,7 +41,7 @@ class ParkingLot:
                 current_time = int(time.time())
                 self.state["slots"][slot_id] = {
                     "car_number": car_number,
-                    "parking_time": current_time
+                    # "parking_time": current_time
                 }
                 self.state["spaces"] = self.state["spaces"]-1
                 
@@ -50,20 +50,21 @@ class ParkingLot:
                 return
 
 
-    def leave_parking_lot(self, car_number):
+    def leave_parking_lot(self, car_number, hours):
         self.load_state()
         # print(car_number)
         car_parking_map={}
         # print(self.state["slots"].items())
         car_parking_map = {value["car_number"]: key for key, value in self.state["slots"].items() if value is not None}
         if car_number in car_parking_map:
-            parked_since=self.state["slots"][car_parking_map[car_number]]["parking_time"] #its a choice between spending one more local variable vs hurting the next person to read this
-            current_time = int(time.time())
-            parked_hours = (current_time-parked_since)/3600 
-            parked_hours_round_up = int(parked_hours)+int(parked_hours % 1 != 0) #or use a math lib 
-            cost = max(10, 10 + (parked_hours_round_up - 2) * 10) #Cars can't literaly leave within a second after parking.
+            # parked_since=self.state["slots"][car_parking_map[car_number]]["parking_time"] #its a choice between spending one more local variable vs hurting the next person to read this
+            # current_time = int(time.time())
+            # parked_hours = (current_time-parked_since)/3600 
+            # parked_hours_round_up = int(parked_hours)+int(parked_hours % 1 != 0) #or use a math lib 
+            cost = max(10, 10 + (hours - 2) * 10) #Cars can't literaly leave within a second after parking.
             print("Registration Number", car_number,"from Slot",car_parking_map[car_number], "has left with Charge", cost)
             self.state["slots"][car_parking_map[car_number]] = None # Marking the slot empty
+            self.state["spaces"] = self.state["spaces"]+1
             self.save_state()
         else:
             print("Registration Number",car_number,"not found")
@@ -71,10 +72,10 @@ class ParkingLot:
 
     def display_parking_lot_status(self):
         self.load_state()
-        print("Parking lot status:"
-            "\nTotal spaces:", len(self.state["slots"]),
-            "\nAvailable spaces:", self.state["spaces"],
-            "\nSlot_Id  Car_number")
+        # print("Parking lot status:"
+        #     "\nTotal spaces:", len(self.state["slots"]),
+        #     "\nAvailable spaces:", self.state["spaces"],
+        print("\nSlot No. Registration No.")
 
         #Making the null finding faster
         non_null_dict = {key: value["car_number"] for key, value in self.state["slots"].items() if value is not None}
@@ -87,31 +88,41 @@ def usage(accepted_arguments):
     print("Usage: python3 script_name.py [command]")
     print("Accepted commands:", ", ".join(accepted_arguments))
  
-def main():
+def main(file):
     # Define the list of accepted arguments
     accepted_arguments = ["create", "park", "leave", "status"]
 
     # Initialize the parking lot object
     parking_lot = ParkingLot()
 
-    # Check if the user input matches the accepted arguments
-    if len(sys.argv) > 1 and sys.argv[1] in accepted_arguments:
-        if sys.argv[1] == "create" and len(sys.argv) == 3:
-            parking_lot.create_parking_lot(sys.argv[2])
+    # Open the file for reading
+    with open(file, 'r') as f:
+        # Read each line of the file
+        for line in f:
+            # Split the line into a list of arguments
+            args = line.strip().split()
 
-        elif sys.argv[1] == "park" and len(sys.argv) == 3:
-            parking_lot.park_car(sys.argv[2])
+            # Check if the user input matches the accepted arguments
+            if len(args) > 0 and args[0] in accepted_arguments:
+                if args[0] == "create" and len(args) == 2:
+                    parking_lot.create_parking_lot(args[1])
 
-        elif sys.argv[1] == "leave" and len(sys.argv) == 3:
-            parking_lot.leave_parking_lot(sys.argv[2])
+                elif args[0] == "park" and len(args) == 2:
+                    parking_lot.park_car(args[1])
 
-        elif sys.argv[1] == "status":
-            parking_lot.display_parking_lot_status()
+                elif args[0] == "leave" and len(args) == 3:
+                    parking_lot.leave_parking_lot(args[1], int(args[2]))
 
-        else:
-            usage(accepted_arguments)
-    else:
-        usage(accepted_arguments)
+                elif args[0] == "status":
+                    parking_lot.display_parking_lot_status()
+
+                else:
+                    usage(accepted_arguments)
+            else:
+                usage(accepted_arguments)
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) == 2:
+        main(sys.argv[1])
+    else:
+        print("Usage: python parking_lot.py <file>")
